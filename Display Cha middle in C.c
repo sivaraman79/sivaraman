408355bdd88b0470660878286ba0c88e0a4e8006
Sivaraman R <sivaramansnl@gmail.com>
2017-10-04 10:23:52 +0530

    Update and rename Display Cha middle in C to Display Cha middle in C.c

---
+++ Display Cha middle in C.c
@@ -0,0 +1,25 @@
+# sivaraman
+#include <stdio.h>
+#include<string.h>
+#include<conio.h>
+int main()
+{
+char str[100];
+int temp;
+printf("\nEnter the string",str);
+scanf("%s",str);
+char delims[] = " ";
+char *result;
+if {
+    temp=strlen(result);
+    if(temp % 2==0)
+    {
+        result = strtok(str, delims );
+    }
+    else
+    {
+        printf("\n ",str[0]);
+    }
+}
+getch();
+}
